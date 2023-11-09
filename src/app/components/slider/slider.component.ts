fb98ee628a39597c5271cea5032df9e48e567852
mustafaguler3 <mustafa.glr@hotmail.com>
2023-11-09 17:07:10 +0300

    slider component

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit{
 
+  movies: any;
+
+  constructor(private http:HttpClient){}
+
+  ngOnInit(): void {
+      this.getPopularMovies()
+  }
+
+  getPopularMovies(){
+    this.http.get("https://api.themoviedb.org/3/movie/popular?api_key=f260570b610e1483f1c983a5b3e2a2e6").subscribe(
+        response => {
+          this.movies = response
+        }
+      )
+  }
 }
